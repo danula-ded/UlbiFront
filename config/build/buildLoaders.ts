ae7c1a5acf56608bda809d4166f3b65b4107037b
danula-ded <stupishin1597@yandex.ru>
2025-04-06 16:08:37 +0300

    5. css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,27 +1,33 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+import loader from "mini-css-extract-plugin/types/loader";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes(".module."),
+            localIdentName: options.isDev
+              ? "[path][name]__[local]--[hash:base64:5]"
+              : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            // Creates `style` nodes from js strings
-            'style-loader',
-            // Translates CSS into CommonJS
-            'css-loader',
-            // Compiles Sass to CSS
-            'sass-loader',
-        ]
-    }
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
 
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
-
-    return [
-        typescriptLoader,
-        cssLoader,
-    ]
+  return [typescriptLoader, cssLoader];
 }
