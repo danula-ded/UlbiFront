7a67a9dfa136f172e21f83b9623456c21a95a515
danula-ded <stupishin1597@yandex.ru>
2025-04-07 23:36:30 +0300

    7. organization style and add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useContext, useState } from "react";
 import Counter from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
+
       <Link to={"/"}>Main</Link>
       <Link to={"/about"}>About</Link>
 
