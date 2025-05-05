cb320bc1d7c0ef394c55b79e7b73ecc0714a548f
danula-ded <stupishin1597@yandex.ru>
2025-05-06 00:06:06 +0300

    14. Sidebar, Layout приложения

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -0,0 +1,32 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import {  } from 'react-i18next';
+import cls from './Sidebar.module.scss';
+import { useState } from 'react';
+import { Button } from 'shared/ui/Button/Button';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+interface SidebarProps {
+    className?: string;
+}
+
+export const Sidebar = ({ className }: SidebarProps) => {
+
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onToggle = () => {
+        setCollapsed((prev) => !prev);
+    };
+
+    return (
+        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
+            <Button
+                onClick={onToggle}
+            >
+                {collapsed ? '>' : '<'}
+            </Button>
+            <div className={cls.switchers}>
+                <ThemeSwitcher />
+                {/* <LangSwitcher /> */}
+            </div>
+        </div>
+    );
+};
