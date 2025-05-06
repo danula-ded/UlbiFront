21c289ec9295b02f9199ac9e8348777443154db8
danula-ded <stupishin1597@yandex.ru>
2025-05-07 01:25:46 +0300

    15. i18n Интернационализация, Define plugin

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import cls from './Sidebar.module.scss';
 import { useState } from 'react';
 import { Button } from 'shared/ui/Button/Button';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { LangSwitcher } from 'shared/ui/LangSwitcher';
 interface SidebarProps {
     className?: string;
 }
@@ -25,7 +26,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
-                {/* <LangSwitcher /> */}
+                <LangSwitcher className={cls.lang}/>
             </div>
         </div>
     );
